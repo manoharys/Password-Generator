b6f9e42f943da8524bf7adcfe1c69e8fab6ab8c4
manohar <manumanohar1527@gmail.com>
2020-04-17 11:24:13 +0530

    added form eventListener

--- app.js
+++ app.js
@@ -6,13 +6,37 @@ const includeUppercase = document.getElementById('includeUppercase');
 const includeLowercase = document.getElementById('includeLowercase');
 const includeSymbols = document.getElementById('includeSymbols');
 const button = document.querySelector('button');
+const form = document.querySelector('form');
 
 charAmountNumber.addEventListener('input',syncValues);
 charAmountRange.addEventListener('input',syncValues);
 
+//Function which syncronises range and input values...
 function syncValues(e){
     let values = e.target.value;
     charAmountNumber.value = values;
-    charAmountRange.value = values;
-    
+    charAmountRange.value = values;    
+}
+
+form.addEventListener('submit',e =>{
+   let charAmount = charAmountNumber.value;
+   let uppercase = includeUppercase.checked;
+   let lowercase =includeLowercase.checked;
+   let symbols = includeSymbols.checked;
+   const password = generatePassword(charAmount,uppercase,lowercase,symbols);
+})
+
+//console.log(String.fromCharCode(66))
+
+//Function which Generate password..
+function generatePassword(charAmount,uppercase,lowercase,symbols){
+      
+}
+
+//Function which charCode array
+let arr = [];
+function lowToHigh(low,high){
+  for(let i=low;i<=high;i++){
+     arr.push(i);
+  }
 }
